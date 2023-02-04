6b9035ad1b67036a1711782e54a9e5caaa77bdd9
“Francisco” <72509322+franpua@users.noreply.github.com>
2023-02-04 21:59:17 +0100

    archivo integrales sin conflictos

--- integrales.py
+++ integrales.py
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 print("esto es una integral que me gusta mucho")
-=======
-print("esto es una integral muy chula")
 print("esto va a entrar en conflicto")
->>>>>>> integrales-modificadas
